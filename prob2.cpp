ef18c51c562e0cb198b291f5197b5bc4646d8b99
Esha Wang <eshawang16@gmail.com>
2015-10-24 21:35:31 -0500

    Added prob2.cpp

---
+++ prob2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fibSum(int n)
+{
+	int prev = 1;
+	int cur = 2;
+	int sum = 0;
+
+	while (prev < n)
+	{
+		int temp = cur;
+		cur = prev + cur;
+		prev = temp;
+		if (prev % 2 == 0) {sum = sum + prev;}
+	}
+
+	return sum;
+}
+
+int main()
+{
+	printf("%d\n", fibSum(4000000));
+	return 0;
+}
