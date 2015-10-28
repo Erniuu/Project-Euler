e1220d60957d81c56a30b6626c3c0a0a952c9551
Esha Wang <eshawang16@gmail.com>
2015-10-28 17:35:49 -0500

    Added prob6.cpp

---
+++ prob6.cpp
@@ -0,0 +1,29 @@
+// Returns the sum of squares of the first n integers.
+int sumOfSquares(int n)
+{
+	int result = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		result = result + i * i;
+	}
+
+	return result;
+}
+
+// Returns the square of sums of the first n integers.
+int squareOfSums(int n)
+{
+	int result = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		result = result + i;
+	}
+
+	return result * result;
+}
+
+int main()
+{
+	printf("%d\n", squareOfSums(100) - sumOfSquares(100));
+	return 0;
+}
