de71fd4f83bb8119d23808a21fa27c69b2fe7ad2
Esha Wang <eshawang16@gmail.com>
2015-11-02 10:12:19 -0600

    Added problem description

--- prob8.cpp
+++ prob8.cpp
@@ -1,3 +1,8 @@
+/* The four adjacent digits in the 1000-digit number that have the greatest 
+product are 9 × 9 × 8 × 9 = 5832. Find the thirteen adjacent digits in the 
+1000-digit number that have the greatest product. What is the value of this 
+product? */
+
 #include <stdio.h>
 
 // Finds the greatest 13-digit product of that 1000-digit long number.
